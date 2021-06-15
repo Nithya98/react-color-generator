2b5e2ea0cea09a2cb0a0ee78776c5abd53737807
Nithya98 <nithyashanmugam03@gmail.com>
2021-06-15 23:29:59 +0530

    removed warnings

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import rgbToHex from './util';
+// import rgbToHex from './util';
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
+  // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
   useEffect(() => {
     const timeout = setTimeout(() => {
